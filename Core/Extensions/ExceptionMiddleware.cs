96f6271bbeb0a2ba9cc07d41f76e083962879030
okanberhoglu <75364004+okanberhoglu@users.noreply.github.com>
2021-04-13 19:26:06 +0300

    13.04.2021

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using FluentValidation.Results;
 
